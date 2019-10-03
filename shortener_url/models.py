40a5bd61e1f3d35e3e2880134ce1a98fc5c6f09c
karolinny <karolinny.silva@gmail.com>
2019-10-03 09:55:25 -0300

    Shortener url

--- shortener_url/models.py
+++ shortener_url/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Url(models.Model):
+    original_url = models.CharField(max_length=255)
+    short_url = models.CharField(max_length=255)
+    custom_alias = models.CharField(max_length=255)
--- shortener_url/views.py
+++ shortener_url/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from shortener_url.models import Url
+import hashlib
+import time
 
-# Create your views here.
+
+@api_view(['POST'])
+def create_shortener(request):
+    
+    try:
+        url = request.data.get('url', False)
+        if url:
+            custom_alias = request.data.get('CUSTOM_ALIAS', False)
+            
+            if custom_alias and Url.objects.filter(custom_alias=custom_alias):
+                return JsonResponse({'alias': custom_alias, 'err_code':'001', 'description': 'CUSTOM ALIAS ALREADY EXISTS'}, status=400)
+            else:
+                start = time.time()
+                if custom_alias:
+                    short_url = "http://shortener/u/{}".format( custom_alias )
+                else:
+                    custom_alias = hashlib.sha256( str( Url.objects.all().count() ).encode('utf-8') ).hexdigest()[:6] 
+                    short_url = "http://shortener/u/{}".format( custom_alias )
+                Url.objects.create(original_url=url, custom_alias=custom_alias, short_url=short_url)
+                end = time.time()
+                return JsonResponse({'url': short_url, 
+                                    'alias': custom_alias, 
+                                    'statistics': {"time_taken": "{} ms".format( end-start ) } }, status=200)
+        else:
+            return JsonResponse({'url': url, 'err_code':'003', 'description': 'URL isrequired'}, status=400)
+    except:
+        return JsonResponse({'Error':'Internal server error :('}, status=500)
